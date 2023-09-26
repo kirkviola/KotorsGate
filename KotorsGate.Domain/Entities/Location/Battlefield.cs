0ab44233b87aeb3749164ce2c10f13abbf0b3d83
Matthew Kirkendall <kirk.viola@gmail.com>
2023-09-26 15:46:48 -0500

    More models made

---
+++ KotorsGate.Domain/Entities/Location/Battlefield.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KotorsGate.Domain.Entities.Location
+{
+    public class Battlefield
+    {
+        public int Id { get; set; }
+        public int LocationId { get; set; }
+
+        public Battlefield() { }
+    }
+}
---
+++ KotorsGate.Domain/Entities/Location/BattlefieldSquare.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KotorsGate.Domain.Entities.Location
+{
+    public class BattlefieldSquare
+    {
+        public int Id { get; set; }
+        public int BattlefieldId { get; set; }
+
+        public BattlefieldSquare() { }
+    }
+}
