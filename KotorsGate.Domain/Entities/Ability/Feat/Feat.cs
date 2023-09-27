e4b91dad1ab7ce1138ae506c2ce350a634a35669
Matthew Kirkendall <kirk.viola@gmail.com>
2023-09-27 09:07:05 -0500

    Removed unused usings

--- KotorsGate.Domain/Entities/Ability/Feat/Feat.cs
+++ KotorsGate.Domain/Entities/Ability/Feat/Feat.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace KotorsGate.Domain.Entities.Ability.Feat
+﻿namespace KotorsGate.Domain.Entities.Ability.Feat
 {
     public class Feat
     {
--- KotorsGate.Domain/Entities/Ability/Power/Power.cs
+++ KotorsGate.Domain/Entities/Ability/Power/Power.cs
@@ -1,9 +1,4 @@
 ﻿using KotorsGate.Domain.Constants;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KotorsGate.Domain.Entities.Ability.Power
 {
