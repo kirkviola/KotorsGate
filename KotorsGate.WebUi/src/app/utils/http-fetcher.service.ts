7bec17010b06a23b2114a9eec9f762edfe6348db
Matthew Kirkendall <kirk.viola@gmail.com>
2023-12-03 18:30:55 -0600

    basic wrapping for http fetcher

--- KotorsGate.WebUi/src/app/utils/http-fetcher.service.ts
+++ KotorsGate.WebUi/src/app/utils/http-fetcher.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -11,17 +11,42 @@ export class HttpFetcherService {
 
   constructor() { }
 
-  get<T>(path: string, params?: QueryParams): Observable<T> {
-    return this.#http.get<T>(`/echo/api/${path}`, {params: this.#buildParams(params)}) as Observable<T>;
+  get<T>(path: string, params?: QueryParams[], headers?: Header[]): Observable<T> {
+    return this.#http.get<T>(`/echo/api/${path}`, {params: this.#buildParams(params), headers: this.#addHeaders(headers)}) as Observable<T>;
+  }
+
+  post<T>(path: string, body: any, params?: QueryParams[], headers?: Header[]): Observable<T> {
+    return this.#http.post<T>(`/echo/api/${path}`, {body: body, params: this.#buildParams(params), headers: this.#addHeaders(headers)}) as Observable<T>;
   }
 
 
-  #buildParams(params?: QueryParams): HttpParams | undefined {
-    return params != null ? new HttpParams().set(params.key, params.value) : undefined;
+  #buildParams(params?: QueryParams[]): HttpParams | undefined {
+    if (params != null) {
+      let queryParams = new HttpParams();
+      params.map(param => queryParams.append(param.key, param.value));
+      return queryParams;
+    }
+
+    return undefined;
+  }
+
+  #addHeaders(headers?: Header[]): HttpHeaders | undefined {
+    if (headers != null) {
+      let httpHeaders = new HttpHeaders();
+      headers.map(header => httpHeaders.append(header.name, header.value));
+      return httpHeaders;
+    }
+
+    return undefined;
   }
 }
 
 export interface QueryParams {
   key: string;
   value: string | boolean | number;
+}
+
+export interface Header {
+  name: string;
+  value: string;
 }
