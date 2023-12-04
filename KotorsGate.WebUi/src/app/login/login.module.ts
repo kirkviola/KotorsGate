c9af44ff90d37b4623fbd70adb28d523b1dcd042
Matthew Kirkendall <kirk.viola@gmail.com>
2023-12-04 15:05:28 -0600

    Routing and some css

--- KotorsGate.WebUi/src/app/login/login.module.ts
+++ KotorsGate.WebUi/src/app/login/login.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
 
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     CommonModule,
+    MatCardModule,
     RouterModule.forChild([
       { path: '', component: LoginComponent },
     ])
--- KotorsGate.WebUi/src/app/shared/top-bar/top-bar.component.ts
+++ KotorsGate.WebUi/src/app/shared/top-bar/top-bar.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LinkConfig, TopBarItemComponent } from './top-bar-item/top-bar-item.component';
 
 @Component({
   selector: 'top-bar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    TopBarItemComponent,
+  ],
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.scss'
 })
-export class TopBarComponent {
+export class TopBarComponent implements OnInit {
 
+  routes: LinkConfig[] = [];
+
+  ngOnInit(): void {
+    // TODO: As more navigation options become available with gated access, push them to the array here
+    const login = { name: 'Login', route: '/login' } satisfies LinkConfig;
+
+    this.routes.push(login);
+
+  }
 }
